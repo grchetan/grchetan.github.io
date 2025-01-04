4719d977593c5c55bda10b3f1d1c5acef6a246dc
Chetan Prajapat <chetanprajapat340@gmail.com>
2025-01-04 14:27:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,35 +1,8 @@
 // Get elements
-const openResumePopupButton = document.getElementById("openResumePopup");
-const resumePopup = document.getElementById("resumePopup");
-const closeResumePopupButton = document.getElementById("closeResumePopup");
 const toggleButton = document.getElementById("darkModeToggle");
 const lightVideo = document.querySelector(".background-video.light-mode");
 const darkVideo = document.querySelector(".background-video.dark-mode");
 const body = document.body;
-
-// Function to open the pop-up
-function openResumePopup() {
-  resumePopup.style.display = "flex";
-  setTimeout(() => resumePopup.classList.add("show"), 10); // Smooth transition
-}
-
-// Function to close the pop-up
-function closeResumePopup() {
-  resumePopup.classList.remove("show");
-  setTimeout(() => (resumePopup.style.display = "none"), 500);
-}
-
-// Event listeners for the pop-up
-openResumePopupButton.addEventListener("click", openResumePopup);
-closeResumePopupButton.addEventListener("click", closeResumePopup);
-window.addEventListener("click", (event) => {
-  if (event.target === resumePopup) {
-    closeResumePopup();
-  }
-});
-
-// Initial setup for toggle button emoji
-toggleButton.classList.add("animate");
 
 // Event listener for the toggle button
 toggleButton.addEventListener("click", () => {
@@ -53,11 +26,4 @@ toggleButton.addEventListener("click", () => {
 // Animation effect for the emoji transition
 toggleButton.addEventListener("animationend", () => {
   toggleButton.classList.remove("animate");
-});
-
-// Navigatio button click
-// Toggle for hamburger menu
-document.getElementById("navtoggle").addEventListener("click", function () {
-  const navLinks = document.getElementById("navLinks");
-  navLinks.classList.toggle("active");
 });
